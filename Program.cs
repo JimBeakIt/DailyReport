1c8bc1b9dce02e59e59302b8d9cb9209e97f25d1
JimBeakIt <87563663+JimBeakIt@users.noreply.github.com>
2021-10-30 10:30:42 +0100

    update

--- Program.cs
+++ Program.cs
@@ -10,6 +10,47 @@ namespace DailyReport
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Pitman Training");
+            Console.ReadLine();
+
+            Console.WriteLine("Student Daily Report");
+            Console.ReadLine();
+
+            Console.WriteLine("What course are you in?");
+            string courseName = Console.ReadLine();
+            Console.WriteLine("Your course is: " + courseName);
+            Console.ReadLine();
+
+            Console.WriteLine("What page number?");
+            string pageNumber = Console.ReadLine();
+            int pagNum = Convert.ToInt32(pageNumber);
+            Console.WriteLine("Your page number is: " + pageNumber);
+            Console.ReadLine();
+
+            Console.WriteLine("Do you need help with anything? Please answer 'true' or 'false'");
+            string studyHelp = Console.ReadLine();
+            bool stuHel = Convert.ToBoolean(studyHelp);
+            Console.WriteLine("Your answered: " + studyHelp);
+            Console.ReadLine();
+
+            Console.WriteLine("Were there any positive experiences you'd like to share? Please provide specifics.");
+            string positiveExp = Console.ReadLine();
+            Console.WriteLine("Your positive experience was: " + positiveExp);
+            Console.ReadLine();
+
+            Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific.");
+            string feedBack = Console.ReadLine();
+            Console.WriteLine("Your feedback: " + feedBack);
+            Console.ReadLine();
+
+            Console.WriteLine("How many hours did you study today?");
+            string studyHours = Console.ReadLine();
+            int stuHou = Convert.ToInt32(studyHours);
+            Console.WriteLine("Your hours studied is: " + studyHours);
+            Console.ReadLine();
+
+            Console.WriteLine("Thank you for your responses. An Instructor will respond shortly. Have a great day!");
+            Console.ReadLine();
         }
     }
 }
